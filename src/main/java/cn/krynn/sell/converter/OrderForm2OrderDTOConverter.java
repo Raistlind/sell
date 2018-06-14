0bb5bd15b9f21a624230982f80f8adac3294dc74
RaistlinD <Johnd0712@gmail.com>
2018-06-14 23:18:27 +0800

    ok

---
+++ src/main/java/cn/krynn/sell/converter/OrderForm2OrderDTOConverter.java
@@ -0,0 +1,44 @@
+package cn.krynn.sell.converter;
+
+import cn.krynn.sell.dataobject.OrderDetail;
+import cn.krynn.sell.dto.OrderDTO;
+import cn.krynn.sell.enums.ResultEnum;
+import cn.krynn.sell.exception.SellException;
+import cn.krynn.sell.form.OrderForm;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RaistlinD
+ * 2018/6/13 下午10:57
+ */
+@Slf4j
+public class OrderForm2OrderDTOConverter {
+
+    public static OrderDTO convert(OrderForm orderForm) {
+        Gson gson = new Gson();
+
+        OrderDTO orderDTO = new OrderDTO();
+
+        orderDTO.setBuyerName(orderForm.getName());
+        orderDTO.setBuyerPhone(orderForm.getPhone());
+        orderDTO.setBuyerAddress(orderForm.getAddress());
+        orderDTO.setBuyerOpenid(orderForm.getOpenid());
+
+        List<OrderDetail> orderDetailList = new ArrayList<>();
+        try {
+            orderDetailList = gson.fromJson(orderForm.getItems(), new TypeToken<List<OrderDetail>>() {
+            }.getType());
+        } catch (Exception e) {
+            log.error("【对象转换】错误， string={}", orderForm.getItems());
+            throw new SellException(ResultEnum.PARAM_ERROR);
+        }
+        orderDTO.setOrderDetailList(orderDetailList);
+
+        return orderDTO;
+    }
+}
