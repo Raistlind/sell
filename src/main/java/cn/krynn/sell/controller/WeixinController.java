4a2c3d5bcc0184f4fc3cd55b84daf399b66e14e9
RaistlinD <Johnd0712@gmail.com>
2018-06-22 23:00:28 +0800

    ok

---
+++ src/main/java/cn/krynn/sell/controller/WeixinController.java
@@ -0,0 +1,23 @@
+package cn.krynn.sell.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by RaistlinD
+ * 2018/6/22 下午10:38
+ */
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeixinController {
+
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code") String code) {
+        log.info("进入auth方法...");
+        log.info("code={}", code);
+    }
+}
